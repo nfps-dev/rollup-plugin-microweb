d6717a3e90e5d4f80e69a5f9cf1af125ecc2ebb4
Blake Regalia <blake.regalia@gmail.com>
2023-05-16 02:02:59 -0700

    chore: fix cjs/esm duality

--- src/main.ts
+++ src/main.ts
@@ -2,74 +2,73 @@ import type {
 	OutputPlugin,
 } from 'rollup';
 
-const resolve = require('@rollup/plugin-node-resolve');
+import resolve from '@rollup/plugin-node-resolve';
 
-const terser = require('@rollup/plugin-terser');
-const typescript = require('@rollup/plugin-typescript');
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
-const filesize = require('rollup-plugin-filesize');
-const uglify = require('uglify-js');
+import filesize from 'rollup-plugin-filesize';
+import uglify from 'uglify-js';
 
 export interface MicroWebConfig {
 	include?: string;
 }
 
-module.exports = {
-	microWeb(gc_microweb: MicroWebConfig={}): OutputPlugin[] {
-		return [
-			// node-style resolution
-			resolve({
-				browser: true,
+export function microWeb(gc_microweb: MicroWebConfig={}): OutputPlugin[] {
+
+	return [
+		// node-style resolution
+		resolve({
+			browser: true,
+		}),
+
+		// enable typescript
+		typescript({
+			sourceMap: true,
+			include: gc_microweb.include || 'src/**/*.ts',
+		}),
+
+		// minify using terser
+		...'development' !== process.env['NODE_ENV']? [
+			terser({
+				compress: {
+					passes: 3,
+					ecma: 2020,
+					// toplevel: true,
+					keep_fargs: false,
+					unsafe_arrows: true,
+					// unsafe_comps: true,
+					unsafe_methods: true,
+					unsafe_proto: true,
+					unsafe_regexp: true,
+
+					// this can cause an error when web crypto API expects a boolean
+					// booleans_as_integers: true,
+				},
+				mangle: {
+					toplevel: true,
+				},
+				format: {
+					wrap_func_args: false,
+				},
 			}),
 
-			// enable typescript
-			typescript({
-				sourceMap: true,
-				include: gc_microweb.include || 'src/**/*.ts',
-			}),
-
-			// minify using terser
-			...'development' !== process.env['NODE_ENV']? [
-				terser({
-					compress: {
-						passes: 3,
-						ecma: 2020,
-						// toplevel: true,
-						keep_fargs: false,
-						unsafe_arrows: true,
-						// unsafe_comps: true,
-						unsafe_methods: true,
-						unsafe_proto: true,
-						unsafe_regexp: true,
-
-						// this can cause an error when web crypto API expects a boolean
-						// booleans_as_integers: true,
-					},
-					mangle: {
-						toplevel: true,
-					},
-					format: {
-						wrap_func_args: false,
-					},
-				}),
-
-				// terser is not perfect on its own, use uglify to clean up remainder
-				{
-					name: 'uglify',
-					generateBundle: {
-						handler(gc_bundle, h_bundle, b_write) {
-							for(const [, g_bundle] of Object.entries(h_bundle)) {
-								if('chunk' === g_bundle.type) {
-									g_bundle.code = uglify.minify(g_bundle.code).code;
-								}
+			// terser is not perfect on its own, use uglify to clean up remainder
+			{
+				name: 'uglify',
+				generateBundle: {
+					handler(gc_bundle, h_bundle, b_write) {
+						for(const [, g_bundle] of Object.entries(h_bundle)) {
+							if('chunk' === g_bundle.type) {
+								g_bundle.code = uglify.minify(g_bundle.code).code;
 							}
-						},
+						}
 					},
-				} as OutputPlugin,
+				},
+			} as OutputPlugin,
 
-				// display the bundled sizes
-				filesize(),
-			]: [],
-		];
-	}
+			// display the bundled sizes
+			filesize(),
+		]: [],
+	];
 }
